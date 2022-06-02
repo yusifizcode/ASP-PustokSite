242d6a5b5e6c125d0c37d9c1dc7be149dff1ae4c
yusifizcode <5ba0gh2@code.edu.az>
2022-06-02 10:20:00 +0400

    Pustok Site strated

---
+++ Pustok/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+//using Pustok.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
